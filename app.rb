d49bee6715848c4d61598d667f34c1ddf27ff78f
Johannes Heck <yoyostile@gmail.com>
2018-05-16 19:59:44 +0200

    add ipv6 support

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Use it like this:
-# https://cloudfritz.r4r3.me/update?key=<pass>&email=<username>&zone=YOURZONE&domain=<domain>&ip=<ipaddr>
+# https://cloudfritz.r4r3.me/update?key=<pass>&email=<username>&zone=YOURZONE&domain=<domain>&ip=<ipaddr>&ipv6=<ip6addr>
 
 require 'sinatra'
 require 'cloudflare'
@@ -18,9 +18,29 @@ end
 
 use Rack::CommonLogger, logger
 
+TYPE_MAPPING = { A: :ip, AAAA: :ipv6 }.freeze
+
+module Cloudflare
+  class DNSRecords < Resource
+    def find_by_params(params)
+      response = get(params: params)
+
+      return if response.empty?
+      record = response.results.first
+
+      DNSRecord.new(concat_urls(url, record[:id]), record, **options)
+    end
+  end
+end
+
 get '/update' do
   connection = Cloudflare.connect(key: params[:key], email: params[:email])
   zone = connection.zones.find_by_name(params[:zone])
-  record = zone.dns_records.find_by_name(params[:domain])
-  record.update_content(params[:ip])
+
+  TYPE_MAPPING.each_key do |type|
+    record = zone.dns_records.find_by_params(name: params[:domain], type: type)
+    value = params[TYPE_MAPPING[type]]
+    record.update_content(value) if value
+  end
+  'Ok.'
 end
